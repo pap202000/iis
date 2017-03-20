4513500aae6222d37d037b6fe8baa09495a8ae25
Paul <pap202000@yahoo.com>
2017-03-20 16:42:24 +0000

    Yeah

---
+++ recipes/server.rb
@@ -0,0 +1,13 @@
+powershell_script 'Install IIS' do
+  	code 'Add-WindowsFeature Web-Server'
+  	action :run
+end
+
+file 'C:\inetpub\wwwroot\Default.htm' do
+	content '<h1>Hello, world!</h1>'
+	action :create
+end
+
+service 'w3svc' do
+	action [ :enable, :start]
+end
